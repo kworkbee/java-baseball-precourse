95fa9531793ad648057f2fd051eb2c91f0fefbbf
G1 <g1.jeon@kakao.com>
2022-01-07 14:35:16 +0900

    feat: Initialize Random Number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,13 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+
 public class Application {
+
+    private static String initRandom() {
+        return String.valueOf(Randoms.pickNumberInRange(123, 987));
+    }
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
     }
