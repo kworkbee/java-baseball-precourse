5fa20e56865098c398374cd58b48ff64af71a74d
G1 <g1.jeon@kakao.com>
2022-01-07 14:37:57 +0900

    feat: Get User Input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,26 @@
 package baseball;
 
+import nextstep.utils.Console;
 import nextstep.utils.Randoms;
 
 public class Application {
 
     private static String initRandom() {
         return String.valueOf(Randoms.pickNumberInRange(123, 987));
+    }
+
+    private static boolean validateUserInput(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+        return userInput.length() == 3;
+    }
+
+    private static String getUserInput() {
+        return Console.readLine();
     }
 
     public static void main(String[] args) {
