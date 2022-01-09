db96616305d0537df6a7f60019d1261d4bba878e
G1 <g1.jeon@kakao.com>
2022-01-10 00:41:24 +0900

    refactor: enum (GameMode)

--- src/main/java/baseball/GameMode.java
+++ src/main/java/baseball/GameMode.java
@@ -1,10 +1,5 @@
 package baseball;
 
 public enum GameMode {
-    ONGOING(0), RENEW(1), STOP(2);
-    final int value;
-
-    GameMode(int enumType) {
-        this.value = enumType;
-    }
+    ONGOING, RENEW, STOP;
 }
